2212c4b961cb6a534d317e1a94a6affeb3b7046e
ht175 <ht175@duke.edu>
2023-04-19 15:17:36 -0400

    add send msg and get msg function

--- back-end/utils.py
+++ back-end/utils.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from db_table import *
+import socket
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _EncodeVarint
 
 
 '''
 @init_engin: Drop all the tables and restart
 '''
+
 
 def init_engine():
     # engine = create_engine(
@@ -15,4 +19,29 @@ def init_engine():
     Base.metadata.drop_all(engine)
     print('Drop tables successfully')
     Base.metadata.create_all(engine)
-    
+
+
+'''
+@sendMessage: encode data length to send and sendmessage to corrosponding socket
+'''
+
+
+def sendMessage(message, socket):
+    msg = message.SerializeToString()
+    _EncodeVarint(socket.sendall, len(msg), None)
+    socket.sendall(msg)
+
+
+'''
+@getMessage: receive data from socket (including get length of real string)
+'''
+def getMessage(socket):
+    var_int_buff = []
+    while True:
+        buf = socket.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_message = socket.recv(msg_len)
+    return whole_message
