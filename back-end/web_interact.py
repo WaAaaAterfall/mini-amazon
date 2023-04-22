a879e77fbf57acd0c65cc051403241932f568eaf
ht175 <ht175@duke.edu>
2023-04-22 14:41:12 -0400

    update web socket

--- back-end/web_interact.py
+++ back-end/web_interact.py
@@ -1,5 +1,7 @@
 from db_table import *
 from utils import *
+from db_connect import *
+PORT = 13145
 
 # '''
 # @findWarehouse: given the order address x,y find the nearst warehouse and update order warehouse
@@ -12,7 +14,7 @@ def findWarehouse(addr_x, addr_y,session):
     nearst_whid = 0
     for warehouse in warehouses:
         x = warehouse.x
-        y = warehouses.y
+        y = warehouse.y
         distance = (x - addr_x)**2 + (y - addr_y)**2
         if distance < min_distance:
             min_distance = distance
@@ -24,12 +26,26 @@ def findWarehouse(addr_x, addr_y,session):
 
 def handle_web(web_fd,session):
     while(True):
-        id = web_fd.recv(4)
-        order = Session.query(Order).filter_by(package_id=id).first()
+        msg = web_fd.recv(1024)
+        data = msg.decode()
+        if not data:
+            continue
+        id = data
+        order = session.query(Order).filter_by(package_id=id).first()
         nearst_whid = findWarehouse(order.addr_x, order.addr_y,session)
         # update order to inclue warehouse id
-        session.query(Order).filter_by(package_id= id ).update({"warehouse_id" : nearst_whid})
+        session.query(Order).filter_by(package_id = id ).update({"warehouse_id" : nearst_whid})
         session.commit()
+
+if __name__ == '__main__':
+    engine = connectDB()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', PORT))
+    sock.listen(100)
+    web_fd, addr = sock.accept()
+    session = getSession(engine)
+    handle_web(web_fd,session)
+
 
 
 
